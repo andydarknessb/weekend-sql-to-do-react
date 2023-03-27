3e396bbad20ac0a34b00c970afe1ab970eddb9c9
Cory Anderson <cory.anderson6@yahoo.com>
2023-03-27 12:02:04 -0500

    get request added

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/api/tasks', async (req, res) => {
+    try{
+        const result = await pool.query
+        res.status(200).json(result.rows);
+    } catch (error) {
+        res.status(500).json({error: error.message });
+    } 
+});
 
 // POST
 
