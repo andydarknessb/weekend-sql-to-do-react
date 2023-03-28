a2099f5829c467b39d905f733c5ef9024980156d
Cory Anderson <cory.anderson6@yahoo.com>
2023-03-28 00:01:53 -0500

    added style_read me_notes

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
 # Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+weekend-sql-to-do-react
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+This project is a simple To-Do list web application that helps users keep track of their tasks. The goal of this project is to provide a simple and intuitive user interface that allows users to easily create, update, and delete tasks.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+The problem this app solves is that sometimes people forget important tasks that they have to do, and often there are multiple tasks that need to be done at different times. This app helps users organize their tasks and stay on top of what they need to do.
+
+To solve this problem, I used React.js to create the front-end interface of the app, and Node.js with Express to create the back-end server that handles the API requests. The app uses a Postgres database to store the tasks, and I used the Postico tool to interact with the database and create the necessary tables.
+
+Prerequisites:
+
+Node.js
+Postgres
+Installation:
+To get the app up and running, follow these steps:
+
+Clone the repository from GitHub: git clone git@github.com:andydarknessb/weekend-sql-to-do-react.git
+Change directory into the project folder: cd to-do-app
+Install the dependencies: npm install
+Create a new Postgres database named to_do_app
+Run the queries in the tables.sql file to create the necessary tables and data
+Start the server: npm run server
+Start the client: npm run client
+The app will now be running at http://localhost:5001
+Usage:
+A user can use the To-Do app as follows:
+
+To add a new task, enter the task description into the input field and click the "Create Task" button
+To mark a task as complete, click the "Mark Complete" button next to the task
+To delete a task, click the "Delete" button next to the task
+Built With:
+
+React.js
+Node.js
+Express
+Postgres
+
+Acknowledgement:
+Thanks to Prime Digital Academy who equipped and helped me to make this application a reality.
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,31 +11,31 @@ function App () {
   }, []);
 
   const fetchTasks = async () => {
-    const response = await axios.get(`/todo`);
-    setTasks(response.data);
+    const response = await axios.get(`/todo`); //fetches tasks from server
+    setTasks(response.data); // sets tasks in state
   };
 
   const createTask = async (e) => {
-    e.preventDefault();
-    if(newTask.trim() === '') return;
-    await axios.post(`/todo`, {description: newTask});
-    setNewTask('');
-    fetchTasks();
+    e.preventDefault(); //prevents page from reloading on submit
+    if(newTask.trim() === '') return; //checks if new task is not empty
+    await axios.post(`/todo`, {description: newTask}); //sends a POST request to server to create a new task
+    setNewTask(''); //resets new task input field
+    fetchTasks(); //fetches updated list of tasks from server
   };
 
   const toggleComplete = async (id, isComplete) => {
-    await axios.put(`/todo/${id}`, {isComplete: !isComplete});
-    fetchTasks();
+    await axios.put(`/todo/${id}`, {isComplete: !isComplete}); //sends a PUT request to server to toggle the is_complete task
+    fetchTasks(); //fetches updated list of tasks from server
   };
 
   const deleteTask = async (id) => {
-    await axios.delete(`/todo/${id}`);
-    fetchTasks();
+    await axios.delete(`/todo/${id}`); //sends a delete request to server to delete the task
+    fetchTasks(); //fetches updated list of tasks from server
   };
 
   const updateTask = async (id, description) => {
-    await axios.put(`/todo/${id}`, { description: description });
-    fetchTasks();
+    await axios.put(`/todo/${id}`, { description: description }); //sends a PUT request to server to update the description of the task
+    fetchTasks(); //fetches updated list of tasks from server
   };
   
   return (
@@ -47,6 +47,7 @@ function App () {
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
           placeholder="Enter a new task"
+          className="task-input"
       />
       <button type="submit">Create Task</button>
       </form>
@@ -54,7 +55,7 @@ function App () {
       {tasks.map((task) => (
           <li key={task.id}>
             {task.is_complete ? (
-              <del>{task.description}</del>
+              <del>{task.description}</del> //strike through task when complete
             ) : (
               <span contentEditable onBlur={(e) => updateTask(task.id, e.target.innerText)}>
                 {task.description}
@@ -64,7 +65,7 @@ function App () {
           <button onClick={() => toggleComplete(task.id, task.is_complete)}>
             {task.is_complete ? 'Mark Incomplete' : 'Mark Complete'}
           </button>
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
+          <button onClick={() => deleteTask(task.id)}>Delete</button> 
         </span>
         </li>
       ))}
